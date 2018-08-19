e1fa71fc0044fb3f8faa7e10b3b760e0a04f6adf
Vitaly <atko@MacBook-Pro-Vitaly.local>
2018-08-19 14:50:38 +0300

    Lesson 2 completed

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Header from '../components/header'
 
 const SecondPage = () => (
+
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
